d1a6181518fd9204c77dc1c9af06316e2420297a
tanzhaowei <tanzw94@gmail.com>
2016-01-07 22:03:54 +0800

    Finish training

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import nltk
 from nltk import bigrams
-s = "I really like python, it's pretty awesome.".split(" ")
-string_bigrams = bigrams(s)
-for grams in string_bigrams:
-	print grams
+
+f = open('cootek_sjtu/train.txt')
+
+d1 = {}
+
+testnum = 0
+for line in f:
+	if line[-1] == '\n':
+		line = line[:-1]
+	l = line.split(' ')
+	del(l[-2])
+	string_bigrams = bigrams(l)
+	for grams in string_bigrams:
+		if not grams[0] in d1:
+			d1[grams[0]] = {}
+		d2 = d1[grams[0]]
+		if not grams[1] in d2:
+			d2[grams[1]] = 0
+		d2[grams[1]] = d2[grams[1]] + 1
