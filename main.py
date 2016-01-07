28e58a35416f9475a3ce4d6085a2d8b928f056ba
tanzhaowei <tanzw94@gmail.com>
2016-01-07 23:39:18 +0800

    naive testing

--- main.py
+++ main.py
@@ -19,3 +19,30 @@ for line in f:
 		if not grams[1] in d2:
 			d2[grams[1]] = 0
 		d2[grams[1]] = d2[grams[1]] + 1
+f.close()
+
+n = 0
+ncorrect = 0
+
+
+f = open('cootek_sjtu/train.txt')
+for line in f:
+	n = n + 1
+	if line[-1] == '\n':
+		line = line[:-1]
+	l = line.split(' ')
+	preword = l[-3]
+	trueword = l[-1]
+	partword = l[-2]
+	if preword in d1:
+		candi = d1[preword]
+		maxn = 0
+		for key in candi:
+			if candi[key] > maxn:
+				maxn = candi[key]
+				chosen = key
+		if chosen == trueword:
+			ncorrect = ncorrect + 1
+
+print n, ncorrect
+f.close()
